170ec04f626407054cb041b023ebfb0b5b2cc33f
奉嘉 <qwer14788lll@163.com>
2021-12-27 14:13:35 +0800

    使用GSON实现对Json数据的解析

--- app/src/main/java/com/example/httpdemo/MainActivity.kt
+++ app/src/main/java/com/example/httpdemo/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.httpdemo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.httpdemo.databinding.ActivityMainBinding
+import com.example.httpdemo.modle.Users
 import com.example.httpdemo.util.HttpUtil
 import com.example.httpdemo.util.JsonUtil
 import com.example.httpdemo.util.XmlUtil
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -143,6 +146,26 @@ class MainActivity : AppCompatActivity() {
             }
         } }
 
-        mBinding.btnJsonGson.setOnClickListener {  }
+        mBinding.btnJsonGson.setOnClickListener {
+            thread {
+                try {
+                    //创建OkHttp客户端对象
+                    val client = OkHttpClient()
+                    //创建Request对象，用来发送HTTP请求
+                    val request = Request.Builder()
+                        .url("http://$ip:8080/json/get_data.json")
+                        .build()
+                    //发出网络请求，并接收回传的数据
+                    val response = client.newCall(request).execute()
+                    //数据解析出来
+                    val data = response.body?.string()
+                    runOnUiThread {
+                        mBinding.textView.text = JsonUtil.parseJsonWithGson(data.toString())
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
